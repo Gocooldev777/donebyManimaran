50a5e753ecaa8272ca9d97b662b37802ad958ab3
Gocooldev777 <79393118+Gocooldev777@users.noreply.github.com>
2022-08-19 01:18:31 +0530

    Update manimaran.js

--- manimaran.js
+++ manimaran.js
@@ -1,16 +1,17 @@
 // INPUT : We are giving bills which contains how much amount each one paid for that perticular transaction
 bills = [ 
-    bill1 = {A:1000,B:2000,C:1000},
-    bill2 = {A:1000,B:250,C:750,D:1250},
-    bill3 = {A:3000,B:100,D:200},
+    bill1 = {A:2000,B:200,C:1000},
+    bill2 = {A:1500,B:0,C:0,D:1300},
+    bill3 = {B:1000,C:0},
+    bill4 = {X:400,Y:3000,Z:400}
 ]
 
 // ***************************************************************************************************************************** 
 
-finalthreebill ={} // this is the final consolidated object it has the values 
+finalThreeBill ={} // this is the final consolidated object it has the values 
 
 // function 1 : sum of values in an object 
-function sumvalues(object){ 
+function sumObjValues(object){ 
     sum = 0 
     for(i in object){ 
         sum = sum + object[i] 
@@ -24,111 +25,114 @@ function objectlength(object){
         length = arr.length  
     }return (length) 
     } 
-// function 3 : sorting objects in ascending order baser upon their values 
-    function sorting(obtosort){ 
-        let sorted_array =[] 
-    for(i in obtosort){ 
-        sorted_array.push([i,obtosort[i]]) 
+// function 3 : sorting objects in ascending order based upon their values 
+    function sorting(objToSort){ 
+        let sortedArray =[] 
+    for(i in objToSort){ 
+        sortedArray.push([i,objToSort[i]]) 
     } 
-    sorted_array.sort((a,b) => a[1] - b[1] ) 
-    sorted_obj = {} 
-    for (mani of sorted_array){ 
-        sorted_obj[mani[0]]=mani[1] 
+    sortedArray.sort((a,b) => a[1] - b[1] ) 
+    sortedObj = {} 
+    for (mani of sortedArray){ 
+        sortedObj[mani[0]]=mani[1] 
     } 
-    return sorted_obj} 
+    return sortedObj} 
 // function 4 : sorting objects in descending order baser upon their values
-function reversesorting(obtosort){ 
-    let sorted_array =[] 
-for(i in obtosort){ 
-    sorted_array.push([i,obtosort[i]]) 
+function reverseSorting(objToSort){ 
+    let sortedArray =[] 
+for(i in objToSort){ 
+    sortedArray.push([i,objToSort[i]]) 
 } 
-sorted_array.sort((a,b) => a[1] - b[1] ).reverse() 
-rev_sorted_obj = {} 
-for (mani of sorted_array){ 
-    rev_sorted_obj[mani[0]]=mani[1] 
+sortedArray.sort((a,b) => a[1] - b[1] ).reverse() 
+revSortedObj = {} 
+for (mani of sortedArray){ 
+    revSortedObj[mani[0]]=mani[1] 
 } 
-return rev_sorted_obj} 
+return revSortedObj} 
 // function 3 : removing key value pairs from an objects if it's value is zero 
-function removezero(zeroobject){ 
-nozeroobj={} 
-for (i in zeroobject){ 
-    if (zeroobject[i] != 0){ 
-        nozeroobj[i]=zeroobject[i] 
+function removeZero(objWithZeroVal){ 
+noZeroObj={} 
+for (i in objWithZeroVal){ 
+    if (objWithZeroVal[i] != 0){ 
+        noZeroObj[i]=objWithZeroVal[i] 
     } 
-}return nozeroobj 
+}return noZeroObj 
 }   
 // ***************************************************************************************************************************** 
-function consolidatebill(bill){     // it gives the consolidated values for each bills 
+function consolidatedBill(bill){     // It gives the consolidated values for each bills 
 // Step : 1 finding sum of the bill  
-sum = sumvalues(bill) 
+sum = sumObjValues(bill) 
 // Step : 2 finding the average  
 length = objectlength(bill) 
 average = sum / length  
-// Step : 3 finding the amount to be paid by each person in the each bill 
-amount_to_be_paid= {} // Example : {A:-100,B:100} "A paid 100 rupees less "
+// Step : 3 finding the amount to be paid by each person in the each bill " amount to be paid = contributed amount - average "
+amountToBePaid= {} // Example : {A:-100,B:100} "A paid 100 rupees less "
 for (let i in bill){ 
-    amount_to_be_paid[i] = ((bill[i]) - average ) 
+    amountToBePaid[i] = ((bill[i]) - average ) 
 } 
-return amount_to_be_paid 
+return amountToBePaid 
 } 
 // Step : 4 summation of all the consolidated bills 
 for (i in bills){ 
-    sep = consolidatebill(bills[i])
+    sep = consolidatedBill(bills[i])
   for (i in sep){
-  if(!(i in finalthreebill)){
-    finalthreebill[i]=sep[i]
+  if(!(i in finalThreeBill)){
+    finalThreeBill[i]=sep[i]
   }
   else{
-    finalthreebill[i]=finalthreebill[i]+sep[i]
+    finalThreeBill[i]=finalThreeBill[i]+sep[i]
   }
 } 
 }
 // ***************************************************************************************************************************** 
 //  Step : 5 finding the amount shoud be paid by each individuals and printing it "Example : A has to give/pay __Rs to B"
-finalthreebill = sorting(finalthreebill)
+finalThreeBill = sorting(finalThreeBill)
 
-to_pay = {} // these people want the perticular amount of money from the concern person since they given more money than the average
-to_get = {} // these people has to pay the perticular amount to the concern person since they given less money then the average
-for (pay in finalthreebill){
-if (finalthreebill[pay]<0){
-    to_pay[pay]=finalthreebill[pay]
+peopleTheyPay = {} //  People want the perticular amount of money from the concern person since they given more money than the average
+peopleTheyGet = {} //  People has to pay the perticular amount to the concern person since they given less money then the average
+
+for (pay in finalThreeBill){
+if (finalThreeBill[pay]<0){
+    peopleTheyPay[pay]=finalThreeBill[pay]
 }
 else{
-    to_get[pay]=finalthreebill[pay]
+    peopleTheyGet[pay]=finalThreeBill[pay]
 }
 }
-to_get = reversesorting(to_get)
+peopleTheyGet = reverseSorting(peopleTheyGet) // Decending order > {person who paid lot will be in the first position}
 
-for (get in to_get){
-for (give in to_pay){
-    if (to_get[get] > Math.abs(to_pay[give])){
-        newval1 = (to_get[get] - Math.abs(to_pay[give]))
-        kudu1 = Math.abs(to_pay[give])
-        to_get[get] = newval1
-        to_pay[give] = 0
-        to_get = removezero(to_get)
-        to_pay = removezero(to_pay)
-        console.log(`${give} has to give the following amount to ${get}`)
-        console.log(kudu1)
+// Settlement part > Person has to pay a bigger amount will give the money to the person who wants bigger amount first
+
+for (nameOfGet in peopleTheyGet){
+for (nameOfPay in peopleTheyPay){
+    if (peopleTheyGet[nameOfGet] > Math.abs(peopleTheyPay[nameOfPay])){
+        newval1 = (peopleTheyGet[nameOfGet] - Math.abs(peopleTheyPay[nameOfPay]))
+        amountToPay1 = Math.abs(peopleTheyPay[nameOfPay])
+        peopleTheyGet[nameOfGet] = newval1
+        peopleTheyPay[nameOfPay] = 0
+        peopleTheyGet = removeZero(peopleTheyGet)
+        peopleTheyPay = removeZero(peopleTheyPay)
+        console.log(`${nameOfPay} has to give the following amount to ${nameOfGet}`)
+        console.log("   Rs.",amountToPay1,"/-")
     }
-    else if(to_get[get] < Math.abs(to_pay[give])){
-        kudu2 = to_get[get]
-        newval2 = (Math.abs(to_pay[give])) - (Math.abs(to_get[get]))
-        to_get[get] = 0
-        to_pay[give] = newval2
-        to_get = removezero(to_get)
-        to_pay = removezero(to_pay)
-        console.log(`${give} has to give the following amount to  ${get}`)
-        console.log(kudu2)
+    else if(peopleTheyGet[nameOfGet] < Math.abs(peopleTheyPay[nameOfPay])){
+        amountToPay2 = peopleTheyGet[nameOfGet]
+        newval2 = (Math.abs(peopleTheyPay[nameOfPay])) - (Math.abs(peopleTheyGet[nameOfGet]))
+        peopleTheyGet[nameOfGet] = 0
+        peopleTheyPay[nameOfPay] = newval2
+        peopleTheyGet = removeZero(peopleTheyGet)
+        peopleTheyPay = removeZero(peopleTheyPay)
+        console.log(`${nameOfPay} has to give the following amount to  ${nameOfGet}`)
+        console.log("   Rs.",amountToPay2,"/-")
     }
-    else if (Math.abs(to_get[get]) == Math.abs(to_pay[give])){
-        kudu3 = to_get[get]
-        to_get[get] = 0
-        to_pay[give] = 0
-        to_get = removezero(to_get)
-        to_pay = removezero(to_pay)
-        console.log(`${give} has to give the following amount to ${get}`)
-        console.log(kudu3)
+    else if (Math.abs(peopleTheyGet[nameOfGet]) == Math.abs(peopleTheyPay[nameOfPay])){
+        amountToPay3 = peopleTheyGet[nameOfGet]
+        peopleTheyGet[nameOfGet] = 0
+        peopleTheyPay[nameOfPay] = 0
+        peopleTheyGet = removeZero(peopleTheyGet)
+        peopleTheyPay = removeZero(peopleTheyPay)
+        console.log(`${nameOfPay} has to give the following amount to ${nameOfGet}`)
+        console.log("   Rs.",amountToPay3,"/-")
     }
 }
 }
